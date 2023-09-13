13b052a9b393edc6b79b473a02b3c45652ca7ab7
curtischan <curtischanyiksum@gmail.com>
2023-09-13 09:23:45 +0800

    complete1

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        System.out.println("Please type in your age:");
+        age = scanner.nextInt();
+        System.out.println("Please type in your first name:");
+        firstName = scanner1.nextLine();
+        System.out.println("Please type in your favourite food:");
+        favouriteFood = scanner2.nextLine();
 
-
+        System.out.println("Age: " + age + "\nName: " + firstName + "\nFavourite Food: " + favouriteFood);
     }
-
 }
