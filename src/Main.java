35e3cb8bacd72979326ab76f9f58c8832316cd72
curtischan <curtischanyiksum@gmail.com>
2023-09-13 09:29:11 +0800

    complete2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
+
         int age;
         String firstName;
         String favouriteFood;
 
-        System.out.println("Please type in your age:");
+        System.out.println("Please enter your age:");
         age = scanner.nextInt();
-        System.out.println("Please type in your first name:");
-        firstName = scanner1.nextLine();
-        System.out.println("Please type in your favourite food:");
-        favouriteFood = scanner2.nextLine();
 
-        System.out.println("Age: " + age + "\nName: " + firstName + "\nFavourite Food: " + favouriteFood);
+        System.out.println("Please enter your first name:");
+        firstName = scanner.next();
+
+        System.out.println("Please enter your favourite food:");
+        favouriteFood = scanner.next();
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Age: " + age);
+        System.out.println("Favourite food: " + favouriteFood);
     }
 }
